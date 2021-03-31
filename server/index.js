77a5fab8a51c1c7115bf0bc392396d2c76bf0ed8
Serhii Komarychev <komarichev@gmail.com>
2021-04-01 00:46:43 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ app.use('/notes', noteRoutes);
 
 const DB_URI = 'mongodb+srv://vinos:19501950Aw@cluster0.eodrz.mongodb.net/notesdb?retryWrites=true&w=majority'
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-console.log('db =>  ...')
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
